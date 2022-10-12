f2b45c21c5fd32b29dc0d9f48c5329be27ce78af
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2022-10-12 22:53:40 +0600

    update

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import './Question.css'
 
 const Question = (props) => {
@@ -6,13 +5,27 @@ const Question = (props) => {
     const { id, question, correctAnswer } = props.question; 
     const {index} = props;
     const options = props.question.options; 
-    // console.log(options);
+    const showAns =(elem)=>{
+        if(correctAnswer === elem){
+            console.log("Correct");
+            document.querySelector('.option').style.backgroundColor="red";
+        }
+        else {
+            console.log("Incorrect")
+        }
+    }
+    // console.log(id);
     return (
         <div className='box'>
-            <h5>Quiz {index}: {question}</h5>
+            <h5 className='question'>Quiz {index}: {question}</h5>
             <div className="options">
-                
-                {options.map(elem => <p className='option'>{elem}</p>)}
+                {/* <input type="radio" name="" id="" /> */}
+                {/* <p className='option'>{elem}</p> */}
+                {options.map((elem,idx) => 
+                    <label className='option'>
+                    <input onClick={() => showAns(elem)} type="radio" name={id} id={elem} value={elem} /> 
+                    <label for={elem}>{elem}</label>
+                    </label>)}
                 
             </div>
             
