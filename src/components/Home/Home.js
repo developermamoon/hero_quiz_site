1ff2aa9b801c698c6ed6dc61cbc4086f03ac6604
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2022-10-12 12:44:31 +0600

    home component completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import './Home.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import image from '../../images/banner5.jpg'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     
     return (
         <div>
             <div className="banner">
-                <div className="left-side"></div>
-                <div className="right-side"></div>
+                <div className="text-part">
+                    <h1>Test your Skills</h1>
+                    <div className='div-btn'>
+                        <Link to='/topics'>
+                            <button className='quiz-btn'>Take a quiz &nbsp; <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon></button>
+                        </Link>
+                    </div>
+                </div>
+                
+                <img src={image} alt="" />
             </div>
         </div>
     );
